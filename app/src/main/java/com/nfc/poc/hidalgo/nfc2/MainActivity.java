8f07739b79b86e474f2cccc66e5770c05d844cda
hidalgo <hidalgoemmanuel@gmail.com>
2018-12-16 17:57:01 +0100

    =dev

--- app/src/main/java/com/nfc/poc/hidalgo/nfc2/MainActivity.java
+++ app/src/main/java/com/nfc/poc/hidalgo/nfc2/MainActivity.java
@@ -3,19 +3,12 @@ package com.nfc.poc.hidalgo.nfc2;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.nfc.NdefMessage;
-import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
-import android.nfc.tech.NfcF;
-import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
